0eb6ac1162774d43869b56da9693d53c5273165c
Ashzad <ashzad.kamal@mediamonks.com>
2024-07-09 13:04:11 +0530

    Updates

---
+++ precache-manifest.b997bdefba8212d0fd3030c41ebfca9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3463a723c6bff4a37fb338e358adc61",
+    "url": "/Object_Detection_WebApp/index.html"
+  },
+  {
+    "revision": "133bccfd8848f4cc77dc",
+    "url": "/Object_Detection_WebApp/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1f9649e575d08dd7e4a9",
+    "url": "/Object_Detection_WebApp/static/js/2.533b0163.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/Object_Detection_WebApp/static/js/2.533b0163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133bccfd8848f4cc77dc",
+    "url": "/Object_Detection_WebApp/static/js/main.24a5bd48.chunk.js"
+  },
+  {
+    "revision": "18e53ccaf3ca473c794d",
+    "url": "/Object_Detection_WebApp/static/js/runtime-main.75d61588.js"
+  }
+]);
